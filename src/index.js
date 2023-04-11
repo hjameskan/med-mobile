707698d5a452ecabfae651e46163321061514175
xshen053 <shenxiaxi26@gmail.com>
2023-04-10 23:17:44 -0700

    added bottom tab

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+import MainTab from './routes';
+
+export default class index extends Component {
+  render() {
+    return <MainTab />;
+  }
+}
+
+const styles = StyleSheet.create({});
